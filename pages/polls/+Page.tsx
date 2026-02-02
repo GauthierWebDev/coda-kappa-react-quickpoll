56020e9d4c9917982327a9f3d1e7f7121c9feeea
GauthierWebDev <gauthier@gauthierdaniels.fr>
2026-02-02 14:06:46 +0100

    ajout de usedebounce

--- pages/polls/+Page.tsx
+++ pages/polls/+Page.tsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+import polls from "../../data/polls.json";
+import { useDebounce } from "../../hooks/useDebounce";
+
 export default function Polls() {
+  const [search, setSearch] = useState("");
+  const debouncedSearch = useDebounce(search, 500);
+
+  const filteredPolls = polls.filter((poll) => {
+    return poll.question.toLowerCase().includes(debouncedSearch.toLowerCase());
+  });
+
   return (
     <main>
       <h1>Catalogue des sondages</h1>
+
+      <label htmlFor="search">Rechercher</label>
+      <input
+        id="search"
+        type="text"
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+      />
+
+      <ul>
+        {filteredPolls.map((poll) => (
+          <li key={poll.id}>{poll.question}</li>
+        ))}
+      </ul>
     </main>
   );
 }
