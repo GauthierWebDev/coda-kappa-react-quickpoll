3cb2d2c7ff7ec0194e7531acc3b422ffafbbc2a7
GauthierWebDev <gauthier@gauthierdaniels.fr>
2026-02-03 16:45:27 +0100

    modification du style global (layout)

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,12 +1,33 @@
 import { usePageContext } from "vike-react/usePageContext";
 
-export function Link({ href, children }: { href: string; children: string }) {
-  const pageContext = usePageContext();
-  const { urlPathname } = pageContext;
-  const isActive = href === "/" ? urlPathname === href : urlPathname.startsWith(href);
+interface LinkProps extends React.HTMLAttributes<HTMLAnchorElement> {
+  href: string;
+}
+
+export function Link({ href, children, ...props }: LinkProps) {
   return (
-    <a href={href} className={isActive ? "is-active" : undefined}>
+    <a href={href} {...props}>
       {children}
     </a>
+  );
+}
+
+export function Navlink({ href, children, ...props }: LinkProps) {
+  const pageContext = usePageContext();
+  const { urlPathname } = pageContext;
+  const isActive =
+    href === "/" ? urlPathname === href : urlPathname.startsWith(href);
+
+  return (
+    <Link
+      href={href}
+      {...props}
+      className={`before:content-['>'] before:mr-1 hover:bg-slate-400/30 rounded-lg px-2 py-1 transition-[colors,translate] ${
+        isActive ? "translate-x-2" : ""
+      } ${props.className}`}
+      aria-current={isActive ? "page" : undefined}
+    >
+      {children}
+    </Link>
   );
 }
--- pages/+Head.tsx
+++ pages/+Head.tsx
@@ -3,5 +3,19 @@
 import logoUrl from "../assets/logo.svg";
 
 export function Head() {
-  return <link rel="icon" href={logoUrl} />;
+  return (
+    <>
+      <link rel="icon" href={logoUrl} />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      />
+      <link
+        href="ttps://fonts.googleapis.com/css2?family=Readex+Pro:wght@160..700&family=Tangerine:wght@400;700&display=swap"
+        rel="stylesheet"
+      />
+    </>
+  );
 }
