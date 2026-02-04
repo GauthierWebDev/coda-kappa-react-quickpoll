c42cc16bb098d29c23c2cdcedb694b5920b52cea
Gauthier Daniels <45950830+GauthierWebDev@users.noreply.github.com>
2026-02-04 10:14:21 +0100

    Style/jour2 (#3)
    * modification du style global (layout)
    
    * style ajouté pour les sondages

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,10 +1,23 @@
+import { Link, Navlink } from "./Link";
+
 export function Nav() {
   return (
-    <nav className="flex flex-col gap-2">
-      <a href="/create">Créer un sondage</a>
-      <a href="/polls">Voir les sondages</a>
-      <a href="/about">À propos</a>
-      <a href="/leaderboard">Classement</a>
-    </nav>
+    <aside className="sticky flex flex-col gap-8 top-4 left-4 m-4 shrink-0">
+      <header className="text-center text-4xl hard-shadow shadow-slate-500 bg-slate-300 px-4 py-2 rounded-lg text-slate-950">
+        <p>
+          <Link href="/">
+            <span className="font-tangerine font-black">Quick</span>
+            <span className="text-slate-700">Poll</span>
+          </Link>
+        </p>
+      </header>
+
+      <nav className="flex flex-col gap-1 bg-slate-300 p-4 rounded-lg hard-shadow shadow-slate-500 text-slate-950">
+        <Navlink href="/create">Créer un sondage</Navlink>
+        <Navlink href="/polls">Voir les sondages</Navlink>
+        <Navlink href="/about">À propos</Navlink>
+        <Navlink href="/leaderboard">Classement</Navlink>
+      </nav>
+    </aside>
   );
 }
--- pages/+Layout.tsx
+++ pages/+Layout.tsx
@@ -1,16 +1,21 @@
-import "./Layout.css";
 import "./tailwind.css";
 
 import { Nav } from "../components/Nav";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex container m-auto">
-      <Nav />
-      <Content>{children}</Content>
+    <div className="bg-slate-800 min-h-dvh w-dvw">
+      <div className="flex container m-auto">
+        <Nav />
+        <Content>{children}</Content>
+      </div>
     </div>
   );
 }
 function Content({ children }: { children: React.ReactNode }) {
-  return <div className="p-4">{children}</div>;
+  return (
+    <div className="m-4 p-4 hard-shadow shadow-slate-500 bg-slate-300 rounded-lg w-full h-max">
+      {children}
+    </div>
+  );
 }
