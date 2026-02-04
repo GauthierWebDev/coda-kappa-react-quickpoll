e1a09877f23ce308b8aaeb9f4dd645b5b141e9aa
GauthierWebDev <gauthier@gauthierdaniels.fr>
2026-02-04 12:01:43 +0100

    dynamisation du titre de la page d'un sondage

--- pages/polls/@id/+data.ts
+++ pages/polls/@id/+data.ts
@@ -1,13 +1,20 @@
 import { render } from "vike/abort";
 import type { PageContextServer } from "vike/types";
+import { useConfig } from "vike-react/useConfig";
 import { pollService } from "@/services/poll.service";
 
 export type Data = Awaited<ReturnType<typeof data>>;
 
 export async function data(pageContext: PageContextServer) {
+  const config = useConfig();
+
   try {
     const { id } = pageContext.routeParams;
     const poll = await pollService.getPollById(Number(id));
+
+    config({
+      title: poll.question,
+    });
 
     return { poll };
   } catch (error) {
