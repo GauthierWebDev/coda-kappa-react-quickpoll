88447846ac1c14d8789400e0e61221f9d67ce8cd
GauthierWebDev <gauthier@gauthierdaniels.fr>
2026-02-05 12:50:04 +0100

    logique basique d'inscription mise en place

--- hooks/useAuth.telefunc.ts
+++ hooks/useAuth.telefunc.ts
@@ -1,3 +1,6 @@
+import { authService } from "@/services/auth.service";
+import { Abort } from "telefunc";
+
 export async function onLogin(email: string, password: string) {
   return { success: true, user: { email, password } };
 }
@@ -7,5 +10,29 @@ export async function onRegister(
   email: string,
   password: string,
 ) {
+  const errors: string[] = [];
+
+  if (name.trim().length < 2) {
+    errors.push("Le nom doit faire au moins 2 caractères");
+  }
+
+  if (!email.includes("@")) {
+    errors.push("Format d'email invalide");
+  }
+
+  if (password.trim().length < 12) {
+    errors.push("Le mot de passe doit faire au moins 12 caractères");
+  }
+
+  if (await authService.getUserByEmail(email)) {
+    errors.push("L'adresse email est déjà utilisée");
+  }
+
+  if (errors.length > 0) {
+    throw Abort({ errorMessage: "Inscription non effectuée", errors });
+  }
+
+  await authService.register(name, email, password);
+
   return { success: true, user: { name, email, password } };
 }
---
+++ services/auth.service.ts
@@ -0,0 +1,33 @@
+import argon2, { hash } from "argon2";
+import { getPrisma } from "@/utils/getPrisma";
+
+export const authService = {
+  async getUserByEmail(email: string) {
+    const prisma = getPrisma();
+    const user = await prisma.user.findUnique({ where: { email } });
+    prisma.$disconnect();
+
+    return user;
+  },
+
+  async register(name: string, email: string, password: string) {
+    const prisma = getPrisma();
+    const hashedPassword = await argon2.hash(password);
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      },
+    });
+    prisma.$disconnect();
+  },
+
+  async hashPassword(clearPassword: string) {
+    return argon2.hash(clearPassword);
+  },
+
+  async verifyPassword(hashedPassword: string, clearPassword: string) {
+    return argon2.verify(hashedPassword, clearPassword);
+  },
+};
